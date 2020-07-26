732e3d86938d3b0f27927aaae12a06d2124c8f94
Jerrad Neff <jerrad.n@gmail.com>
2020-07-25 17:27:13 -0700

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController() { }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
